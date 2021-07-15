8d24a838bb2797f772d11a85c046abd804e1810f
Kirill Ki <kirillkalapov@gmail.com>
2021-07-15 21:23:45 +0200

    First step code

--- ColorMixer/ViewController.swift
+++ ColorMixer/ViewController.swift
@@ -7,7 +7,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UITextFieldDelegate {
 
     
     @IBOutlet weak var colorView: UIView!
@@ -35,25 +36,65 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var greenTextField: UITextField!
     
-    @IBOutlet weak var blueTextField: UILabel!
+    
+    @IBOutlet weak var blueTextField: UITextField!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        red.text = "Red"
+        green.text = "Green"
+        blue.text = "Blue"
+        
+        blueCounter.text = "0"
+        redCounter.text = "0"
+        greenCounter.text = "0"
+        
+      //  redCounter.text = String(redSlider.value)
+        
+        
+     //   colorView.backgroundColor = .red
+        
+        
+        
+        //blueTextField.text = "Hey"
+        
     }
-
     
     
     
     
     @IBAction func redSliderAction(_ sender: UISlider) {
+        
+        
+        let redValue = lround(Double(redSlider.value))
+        print(redValue)
+        
+        redCounter.text = String(redValue)
+        
+        redTextField.text = String(redValue)
+        
+        
     }
     @IBAction func greenSliderAction(_ sender: UISlider) {
+        
+        let greenValue = lround(Double(greenSlider.value))
+        print(greenValue)
+        
+        greenCounter.text = String(greenValue)
+        greenTextField.text = String(greenValue)
     }
     
     @IBAction func blueSliderAction(_ sender: UISlider) {
+        
+        let blueValue = lround(Double(blueSlider.value))
+        print(blueValue)
+        
+        blueCounter.text = String(blueValue)
+        
+        blueTextField.text = String(blueValue)
+        
     }
     
     @IBAction func redTextFieldAction(_ sender: Any) {
